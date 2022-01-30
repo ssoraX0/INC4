8a50e45475789e022259246ef88cb5e296a6ef85
Maria Yahontova <mariayahontova@MacBook-Air-Maria.local>
2022-01-30 16:18:45 +0200

    main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+// 2nd HomeWork for INC4   
+//  https://inc4.net/what-can-cedefi-bring-to-the-crypto-industry/
+// 1.Вытянуть домен из названия сайта   
+const url =  'https://inc4.net/what-can-cedefi-bring-to-the-crypto-industry/'
+
+let domen 
+console.log(domen)
